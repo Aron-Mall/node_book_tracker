c56f8d989796c1bee42c6ad4497d08221a645375
Aron-Mall <58393111+Aron-Mall@users.noreply.github.com>
2021-04-09 14:35:01 +0100

    add the 'add' command

---
+++ book.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const addBook = (title) => {
+  console.log(`adding the book ${title})
+}
+
+
+module.exports = {
+  addBook: addBook
+}
--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const yargs = require('yargs');
+const yargs = require("yargs");
+const chalk = require("chalk");
+const books = require("./book.js");
 
+//adding a book with add command
+
+yargs.command({
+  command: "add",
+  describe: "Add a book",
+  builder: {
+    title: { 
+      describe: "title of book", 
+      demandOption: true, type: 
+      "string" 
+    },
+  },
+  handler: function (argv) {
+    books.addBook(argv.title);
+  },
+});
+
+console.log(yargs.argv);
